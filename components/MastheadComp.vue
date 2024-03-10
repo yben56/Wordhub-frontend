f0ff94d9ead38374add59443e6682cbc2f76b80c
DESKTOP-U6EJ3LB\benjamin <knockknockinfo@gmail.com>
2024-03-10 23:19:55 +0800

    masthead

---
+++ components/MastheadComp.vue
@@ -0,0 +1,73 @@
+<template>
+    <table class="masthead">
+        <tr>
+            <td :class="{ 'd-none': !search, 'd-sm-block': search }"><a href="/"><img class="logo" src="/images/logo.png" /></a></td>
+            <td>
+                <div class="searchbar">
+                    <form action="/result" :class="{ 'd-none': search, 'd-sm-block': search }">
+                        <div class="input-group input-group-sm">
+                            <button @click="ToggleSearch" class="btn d-inline-block d-sm-none" type="button">
+                                <i class="fa-solid fa-arrow-left"></i>
+                            </button>
+                            <input class="form-control" name="search" :placeholder="$t('Search')" type="text" />
+                            <button class="btn btn-outline-secondary" type="submit">
+                                <i class="fa-solid fa-magnifying-glass"></i>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </td>
+            <td :class="{ 'd-none': !search, 'd-sm-block': search }">
+                <i @click="ToggleSearch" class="btn d-inline-block d-sm-none fa-solid fa-magnifying-glass magnifying-glass"></i>
+                <a class="firstname">Benjamin</a>
+                <MenuComp />
+            </td>
+        </tr>
+    </table>  
+</template>
+
+<script setup>
+const search = ref(true)
+
+function ToggleSearch() {
+  search.value = !search.value
+}
+
+onMounted(() => {
+    window.addEventListener('resize', () => {
+        if ( window.innerWidth >= 768 ) {
+            search.value = true
+        }
+    })
+})
+</script>
+
+<style scoped lang="scss">
+.masthead {
+    width: 100%;
+    margin-top: 5px;
+    border-bottom: solid 1px #eee;
+
+    td {
+        text-align: right;
+        padding-bottom: 15px;
+    }
+
+    td:first-child {
+        text-align: left;
+    }
+  
+    .firstname {
+        margin: 0 10px;
+        line-height: 28px;
+    }
+
+    button {
+      border: solid 1px #ccc;
+    }
+
+    input:focus, button:focus {
+      box-shadow: none !important;
+    }
+}
+</style>
---
+++ components/MenuComp.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="menucomp">
+		<div class="btn-group">
+			<button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+				<i class="fa-solid fa-bars"></i> 
+				{{ $t('Menu') }}
+			</button>
+			<ul class="dropdown-menu">
+				<li><router-link class="dropdown-item" to="/">{{ $t('Index') }}</router-link></li>
+				<li><hr class="dropdown-divider"></li>
+				<li><a class="dropdown-item" href="#" @click="Logout()">{{ $t('Logout') }}</a></li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script setup>
+const nuxtApp = useNuxtApp()
+const token = useCookie('token').value
+
+const Logout = () => {
+	nuxtApp.Logout(token)
+}
+</script>
+
+<style scoped>
+.menucomp {
+	float: right;
+}
+</style>
