32da5720556848f16dce69728775e0ffa61e6c25
DESKTOP-U6EJ3LB\benjamin <knockknockinfo@gmail.com>
2024-03-20 01:20:15 +0800

    update layout

--- components/QuizsComp.vue
+++ components/QuizsComp.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-for="(data, index) in props.data">
         <div class="card">
-            <h1 class="from">{{ data.from }}<i class="fa-solid fa-volume-high"></i></h1>
+            <h1 class="from">{{ data.from }}<i @click="$prounce(data.to_prounce)" class="fa-solid fa-volume-high"></i></h1>
             
             <form class="answer" :id="data.id">
                 <div class="question">
