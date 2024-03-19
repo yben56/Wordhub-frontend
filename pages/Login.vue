a9b3f3e3c49a8f72d71033307fcbc213a16ae3f4
DESKTOP-U6EJ3LB\benjamin <knockknockinfo@gmail.com>
2024-03-19 23:11:05 +0800

    update login, signup page css

--- pages/Login.vue
+++ pages/Login.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-lg-4 offset-lg-4">
                 <div class="card mt-5">
-                    <h5 class="card-title p-3 border-bottom">
+                    <h5 class="card-title p-3">
                         {{ $t('Login') }}
                     </h5>
                     <div class="card-body">
@@ -24,7 +24,12 @@ if ( token ) { router.replace('/') }
 
 <style scoped lang="scss">
 .card {
+    border: solid 1px #555;
     background-color: transparent;
     color: #fff;
+
+    .card-title {
+        border-bottom: solid 1px #555;
+    }
 }
 </style>
