d14df65f72815b95e39f0d92e704d3d2d818bd69
DESKTOP-U6EJ3LB\benjamin <knockknockinfo@gmail.com>
2024-06-09 21:07:29 +0800

    update index, scroll bottom load data

--- pages/index.vue
+++ pages/index.vue
@@ -9,37 +9,58 @@
                 <QuizComp v-if="item.type === 'quiz'" :data="[item]"/>
             </template>
         </div>
+        <div id="observer"></div>
     </div>
 </template>
 
 <script setup>
 const { $authorization, $backendapi } = useNuxtApp()
-
+const route = useRoute()
 const data = ref([])
-const quiz = ref([])
-const page = ref(1)
+const classification = route.query.classification
 
 onMounted( async () => {
-    let api_words = await $backendapi('GET', '/api/words?pages=9&page=' + page.value)
+    //1. fetch api data
+    data.value = await fetchData(classification)
+
+    //2. scroll to bottom and load data
+    const observer = new IntersectionObserver((enteries) => {
+        enteries.forEach(async (entry) => {
+            let newdata = await fetchData(classification)
+            data.value = data.value.concat(newdata)
+        })
+    })
+
+    observer.observe(document.getElementById('observer'))
+})
+
+const fetchData = async (classification) => {
+    //1. classification
+    if ( typeof classification === 'undefined' ) { classification = '' }
+    else { classification = '&classification=' + classification }
+
+    //2. words
+    let api_words = await $backendapi('GET', '/api/words?pages=9' + classification)
     api_words.data = api_words.data.map(word => ({type: 'word',...word}))
 
+    //3. quiz
     let api_quiz = await $backendapi('GET', '/api/quiz?pages=3')
     api_quiz.data = api_quiz.data.map(quiz => ({type: 'quiz',...quiz}))
     
-    data.value = mergeArrays(api_words.data, api_quiz.data)
-})
+    //4. merge
+    return mergeArrays(api_words.data, api_quiz.data)
+}
 
 function mergeArrays(arr1, arr2) {
-        let mergedArray = [...arr1]
-        let insertIndex = 3
+    let mergedArray = [...arr1]
+    let insertIndex = 3
 
-        arr2.forEach((item, index) => {
-            mergedArray.splice(insertIndex + index * 4, 0, item);
-        })
+    arr2.forEach((item, index) => {
+        mergedArray.splice(insertIndex + index * 4, 0, item);
+    })
 
-        return mergedArray
-    }
-
+    return mergedArray
+}
 </script>
 
 <style scoped lang="scss">
