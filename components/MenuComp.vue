84d860772b32d70dd659fa36d520026c0c42ebb6
DESKTOP-U6EJ3LB\benjamin <knockknockinfo@gmail.com>
2024-06-09 15:11:26 +0800

    update menu

--- components/MenuComp.vue
+++ components/MenuComp.vue
@@ -6,9 +6,11 @@
 				{{ $t('Menu') }}
 			</button>
 			<ul class="dropdown-menu">
-				<li><a class="dropdown-item" to="/">{{ $t('Index') }}</a></li>
+				<li><a class="dropdown-item" to="/"><i class="fa-solid fa-clock-rotate-left"></i> {{ $t('SearchHistory') }}</a></li>
+				<li><a class="dropdown-item" to="/"><i class="fa-solid fa-chart-simple"></i> {{ $t('LearningStatistic') }}</a></li>
+				<li><a class="dropdown-item" to="/"><i class="fa-solid fa-pen-to-square"></i> {{ $t('OpenEdit') }}</a></li>
 				<li><hr class="dropdown-divider"></li>
-				<li><a class="dropdown-item" @click="signOut({ callbackUrl: '/' })">{{ $t('Logout') }}</a></li>
+				<li><a class="dropdown-item" @click="signOut({ callbackUrl: '/' })"><i class="fa-solid fa-right-from-bracket"></i> {{ $t('Logout') }}</a></li>
 			</ul>
 		</div>
 	</div>
@@ -24,6 +26,7 @@ const { signOut } = useAuth()
 
 	a:hover {
 		background-color: transparent;
+		color: #6610f2;
 	}
 
 }
