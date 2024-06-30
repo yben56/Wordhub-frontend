e6195bf16f6ca4130b595be7507c03396142a5ee
DESKTOP-U6EJ3LB\benjamin <knockknockinfo@gmail.com>
2024-06-30 23:15:34 +0800

    update search include elasticsearch

--- pages/Search.vue
+++ pages/Search.vue
@@ -2,17 +2,20 @@
     <div id="search" class="container">
         <div class="row">
             <div class="col-md-9">
-                <div v-if="result">
-                    <h5><i class="fa-solid fa-magnifying-glass"></i>{{ $t('Search') }}</h5>
-                    <SearchComp :data="search"/>
-                    <h5><i class="fa-solid fa-link"></i>{{ $t('Associate') }}</h5>
-                    <SearchComp :data="associate"/>
-                </div>
-                <div class="p-3" v-else>
-                    <h4 class="text-primary"><h4 class="fa-solid fa-robot"></h4> {{ $t('NoSearchResult') }}...</h4>
+                <div v-if="!result">
+                    <h4 class="text-primary ms-4"><h4 class="fa-solid fa-robot"></h4> {{ $t('NoSearchResult') }}...</h4>
                     <div class="alert alert-info">
                         <p><b>Wordhub</b> <span v-html="$t('NoSearchResultInfo')"></span></p><br>
                         <a href="#" class="addword text-danger"><i class="fa-solid fa-folder-plus"></i> {{ $t('AddWord') }}</a>
+                    </div>
+                </div>
+                <div class="mt-4">
+                    <h5 v-if="result"><i class="fa-solid fa-magnifying-glass"></i>{{ $t('Search') }}</h5>
+                    <h5 v-else><i class="fa-solid fa-magnifying-glass"></i>{{ $t('SearchFuzzy') }}</h5>
+                    <SearchComp :data="search"/>
+                    <div v-if="result">
+                        <h5><i class="fa-solid fa-link"></i>{{ $t('Associate') }}</h5>
+                        <SearchComp :data="associate"/>
                     </div>
                 </div>
             </div>
@@ -36,7 +39,9 @@ const result = ref(true)
 onMounted( async () => {
     //1. fetch api data
     let api_search = await $backendapi('GET', '/api/search/' + useRoute().query.q)
-    if ( api_search.data.length == 0 ) {
+    search.value = api_search.data
+
+    if ( api_search.elasticsearch == true ) {
         result.value = false
     } else {
         //2. if word found, fetch associate
@@ -78,11 +83,9 @@ onMounted( async () => {
 
         .alert-info {
             margin-top: 15px;
-            max-width: 500px;
         }
 
         .addword {
-        
             i {
                 font-size: 18px;
             }
