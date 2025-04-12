264d936e21020a79afd405c8cfa196b91827e994
DESKTOP-U6EJ3LB\benjamin <knockknockinfo@gmail.com>
2025-04-12 23:50:48 +0800

    comment rubbish pages

--- pages/Protected.vue
+++ pages/Protected.vue
@@ -1,13 +1,18 @@
 <template>
+    <p>...</p>
+    <!--
     <span>protected page</span>
     <pre>{{ getSession }}</pre>
     <b>Authorization: </b><span>Bearer {{ getSession.user.access_token }}</span>
+    -->
 </template>
 
 <script setup>
+/*
 definePageMeta({ middleware: 'auth' })
 
 const { data: getSession } = useAuth()
 
 console.log(getSession.value.user.access_token)
+*/
 </script>
